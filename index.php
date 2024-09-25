1525547c399eeb198b9f8e5db766d8f6a056b2ff
Daniel Hernandez Puerto <sirdanielsan64@gmail.com>
2024-09-25 22:16:54 +0200

    Inicializando y vinculando el repositorio

---
+++ index.php
@@ -0,0 +1,13 @@
+<h1>
+<?= "My primera app"; ?>
+</h1>
+
+<style>
+    :root{
+        color-scheme: ligth dark ;
+    }
+    body{
+        display: grid;
+        place-content: center;
+    }
+</style>
